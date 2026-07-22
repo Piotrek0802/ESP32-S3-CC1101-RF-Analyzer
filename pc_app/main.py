e5b417de37b6b6ff3804cd10600bdac7f36e3f34
Piotrek <PiotrekWojcik0802@gmail.com>
2026-07-22 21:04:29 +0200

    Save initial v1.0 prototype code

--- pc_app/main.py
+++ pc_app/main.py
@@ -6,27 +6,40 @@ import statistics
 
 baundRate = 115200
 data=[]
+dataTime=[]
 
 def main():
-    allSerialPorts()
-    comNumb = str(input("COM_ :"))
-    print("Wybrales COM",comNumb)
-    listenPort(comNumb)
+    printAllSerialPorts()
+    
+    listenPort()
 
 
-def allSerialPorts():
+def printAllSerialPorts():
     ports=serial.tools.list_ports.comports()
     for i in ports:
         print(i)
 
-def listenPort(portNum):
+def listenPort():
     global data
-    ser=serial.Serial("COM"+portNum,baundRate,timeout=1)
+    global dataTime
+    
+    while(True):
+        
+        portNumb = str(input("COM_ :"))
+        print("Wybrales COM",portNumb)
+        
+        try:
+            ser=serial.Serial("COM"+portNumb,baundRate,timeout=1)
+            break
+        except:
+            print("Nie mozna otworzyc wybranego portu COM!\n")
+            
     while True:
         newRead=ser.readline()
         newRead.decode("utf-8")
         if newRead:
             data.append(newRead)
+            dataTime.append(datetime.datetime.now().strftime("[%Y:%m:%d-%H:%M:%S.%f]")[:-3] + "] --- ")
             print(data)
         
         if msvcrt.kbhit():
@@ -36,9 +49,10 @@ def listenPort(portNum):
                 saveData()
                 break
 
-def saveData(fileName="plik"):
+def saveData(fileName="pc_app\\reads\\"):
+    fileName=fileName+datetime.datetime.now().strftime("%Y-%m-%d--%H-%M")
     with open(fileName+".txt", "a", encoding="utf-8") as file:
-        for line in data:
-            file.write(datetime.datetime.now().strftime("[%Y:%m:%d-%H:%M:%S.%f]")[:-3] + "] --- "+line.decode("utf-8")+"\n")
-    
+        for line1, line2 in zip(data,dataTime):
+            file.write(line2+line1.decode("utf-8")+"\n")
+    file.close()
 main()
